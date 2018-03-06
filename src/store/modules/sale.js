b371a66ea696fcee84441e58357cccad4924ab50
Lucas <lucassdeabreu@gmail.com>
2018-03-05 21:20:50 -0300

    report

--- src/store/modules/sale.js
+++ src/store/modules/sale.js
@@ -74,6 +74,12 @@ export default {
         }
         context.commit('updateOne', result)
       })
+    },
+    report (context, data) {
+      window.axios.post('/sales/report/', data).then(Response => {
+        Response.data = Response.data.data.map(element => ({ id: element._id, code: element.code, client: element.client, date: element.date, value: element.value }))
+        context.commit('insertAll', Response.data)
+      })
     }
   }
 }
